04138f0ffef1c1c4a35eee62f098e3d92e891b67
DirtyVbif <funkybboy@mail.ru>
2018-11-26 13:47:41 +0300

    modified for lesson-02

--- js/script.js
+++ js/script.js
@@ -1,18 +1,24 @@
 //кнопки показа кода на странице
 function showCode(task) {
-  let task1_codeBlock = document.getElementById('task4_codeBlock');
-  let task1_codeWrap = document.getElementById('task4_codeWrap');
-  let task2_codeBlock = document.getElementById('task5_codeBlock');
-  let task2_codeWrap = document.getElementById('task5_codeWrap');
+  let task1_codeBlock = document.getElementById('task1_codeBlock');
+  let task1_codeWrap = document.getElementById('task1_codeWrap');
+  let task4_codeBlock = document.getElementById('task4_codeBlock');
+  let task4_codeWrap = document.getElementById('task4_codeWrap');
+  let task5_codeBlock = document.getElementById('task5_codeBlock');
+  let task5_codeWrap = document.getElementById('task5_codeWrap');
   
   switch(task) {
-    case 'task4':
+    case 'task1':
       task1_codeBlock.classList.toggle('codeBlock_hidden');
       task1_codeWrap.classList.toggle('taskCard__codeWrap_hidden');
       break;
+    case 'task4':
+      task4_codeBlock.classList.toggle('codeBlock_hidden');
+      task4_codeWrap.classList.toggle('taskCard__codeWrap_hidden');
+      break;
     case 'task5':
-      task2_codeBlock.classList.toggle('codeBlock_hidden');
-      task2_codeWrap.classList.toggle('taskCard__codeWrap_hidden');
+      task5_codeBlock.classList.toggle('codeBlock_hidden');
+      task5_codeWrap.classList.toggle('taskCard__codeWrap_hidden');
       break;
   }
 }
