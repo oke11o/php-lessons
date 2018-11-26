573cfa620b4f27922fb8dc2746a11a3e1600544c
DirtyVbif <funkybboy@mail.ru>
2018-11-26 16:52:03 +0300

    modified for task 6

--- js/script.js
+++ js/script.js
@@ -2,14 +2,21 @@
 function showCode(task) {
   let task1_codeBlock = document.getElementById('task1_codeBlock');
   let task1_codeWrap = document.getElementById('task1_codeWrap');
+
   let task2_codeBlock = document.getElementById('task2_codeBlock');
   let task2_codeWrap = document.getElementById('task2_codeWrap');
+
   let task3_codeBlock = document.getElementById('task3_codeBlock');
   let task3_codeWrap = document.getElementById('task3_codeWrap');
+
   let task4_codeBlock = document.getElementById('task4_codeBlock');
   let task4_codeWrap = document.getElementById('task4_codeWrap');
+
   let task5_codeBlock = document.getElementById('task5_codeBlock');
   let task5_codeWrap = document.getElementById('task5_codeWrap');
+
+  let task6_codeBlock = document.getElementById('task6_codeBlock');
+  let task6_codeWrap = document.getElementById('task6_codeWrap');
   
   switch(task) {
     case 'task1':
@@ -36,6 +43,11 @@ function showCode(task) {
       task5_codeBlock.classList.toggle('codeBlock_fewHidden');
       task5_codeWrap.classList.toggle('taskCard__codeWrap_hidden');
       document.location.href = "#task5_location";
+      break;
+    case 'task6':
+      task6_codeBlock.classList.toggle('codeBlock_hidden');
+      task6_codeWrap.classList.toggle('taskCard__codeWrap_hidden');
+      document.location.href = "#task6_location";
       break;
   }
 }
