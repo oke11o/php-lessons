48c7f478ee9e068e6422e3fd4c3b009713acb5fb
DirtyVbif <funkybboy@mail.ru>
2018-12-02 23:43:18 +0300

    add nav bar menu ass outsource module

---
+++ modules/nav-bar.php
@@ -0,0 +1,46 @@
+<nav class="navBar">  
+  <?php include "nav-bar-menu.php"; ?>
+
+  <a href="<?php echo($navBarMenuArray['main']['href']) ?>" class="navBar__logo">
+    <div></div>
+    <span>site.ru</span>
+  </a>
+
+  <ul class="navBar__menu navBar__menu_margin">
+    <?php
+    foreach ($navBarMenuArray as $navBarItemKey => $navBarItem) {
+      switch (array_key_exists('subMenu', $navBarItem)) {
+        case true: 
+          $isSubMenuSet = ' sub-menu';
+          break;
+        default: $isSubMenuSet = '';
+      }
+
+      switch ($navBarItemKey) {
+        case 'main':
+          $navBarItemClass = 'navBar__item navBar__item_active';
+          break;
+        default: $navBarItemClass = 'navBar__item';
+      }
+
+      echo('<li class="navBar__menuItem'.$isSubMenuSet.'">');
+      echo('<a class="'.$navBarItemClass.'" href="'.$navBarItem['href'].'">'.$navBarItem['name'].'</a>');
+
+      if (isset($navBarItem['subMenu'])) {
+        echo('<div class="subMenu__wrap">
+        <ul class="subMenu subMenu_hidden">');
+
+        foreach ($navBarItem['subMenu'] as $navBarSubItem) {
+          echo('<li class="subMenu__item">');
+          echo('<a href="'.$navBarSubItem['href'].'" class="subMenu__itemHref">'.$navBarSubItem['name'].'</a>');
+          echo('</li>');
+        }
+
+        echo('</ul></div>');
+      }
+
+      echo('</li>');
+    }
+    ?>
+  </ul>
+</nav>
