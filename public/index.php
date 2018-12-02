049f604ad75c5f1a032fd61ed17c52ddbffd0a54
DirtyVbif <funkybboy@mail.ru>
2018-12-02 23:42:13 +0300

    modified nav bar menu

--- public/index.php
+++ public/index.php
@@ -4,153 +4,33 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php
-    $title = 'php-lesson-03';
-    echo "<title>$title</title>";
-    ?>
+  <title>PHP lesson 04</title>
   <link rel="stylesheet" href="css/style.css">
   <link rel="icon" href="dmicon.ico">
+  <?php  
+  /*
+  * --- reserved classes list ---
+  * .sub-menu
+  *   resereved for js to initialize menu with dropdown sub-menu
+  */
+  error_reporting(E_ALL);
+  ?>
 </head>
 
 <body>
   <div class="bodyWrap bodyWrap_position">
     <header>
+
       <?php
-      $headTitle = 'Домашнее задание по курсу PHP. Урок 3.';
-      switch((int)date('n'))
-      {
-        case 1:
-          $month = 'января ';
-          break;
-        case 2:
-          $month = 'февраля ';
-          break;
-        case 3:
-          $month = 'марта ';
-          break;
-        case 4:
-          $month = 'апреля ';
-          break;
-        case 5:
-          $month = 'мая ';
-          break;
-        case 6:
-          $month = 'июня ';
-          break;
-        case 7:
-          $month = 'июля ';
-          break;
-        case 8:
-          $month = 'августа ';
-          break;
-        case 9:
-          $month = 'сентября ';
-          break;
-        case 10:
-          $month = 'октября ';
-          break;
-        case 11:
-          $month = 'ноября ';
-          break;
-        case 12:
-          $month = 'декабря ';
-          break;
-        default: $month = ' день сола ';
-      }
-      $date = date('j').'&nbsp;'.$month.'<br>'.date('Y').'&nbsp;года.';
-      echo "<h1 class='headerTitle'>$headTitle</h1>
-        <p class='headerDate'>$date</p>";
-      ?>
+      include "../modules/nav-bar.php";
+      include "../core/main-date.php";
+      ?>      
+
+      <h1 class='headerTitle'>Домашнее задание по курсу PHP. Урок 4.</h1>
+      <p class='headerDate'><?php echo($date); ?></p>
     </header>
 
     <main class="bodyWrap__main">
-
-      <?php
-      //переменные с названиями и ссылками пунктов меню
-      $menu_item1 = 'главная'; $href_item1 = '#1'; //пункт меню
-      
-      $menu_item2 = 'каталог'; $href_item2 = '#2'; //пункт меню
-      $subMenu2_item1 = 'перейти&nbsp;в&nbsp;каталог'; $subMenu2_href1 = '#2-1'; //пункт подменю
-      $subMenu2_item2 = 'новое&nbsp;в&nbsp;каталоге'; $subMenu2_href2 = '#2-2'; //пункт подменю
-
-      $menu_item3 = 'о&nbsp;нас'; $href_item3 = '#3'; //пункт меню
-      $subMenu3_item1 = 'команда'; $subMenu3_href1 = '#3-1'; //пункт подменю
-      $subMenu3_item2 = 'портфолио'; $subMenu3_href2 = '#3-2'; //пункт подменю
-      
-      $menu_item4 = 'контакты'; $href_item4 = '#4'; //пункт меню
-      $subMenu4_item1 = 'адреса'; $subMenu4_href1 = '#4-1'; //пункт подменю
-      $subMenu4_item2 = 'обратная&nbsp;связь'; $subMenu4_href2 = '#4-2'; //пункт подменю
-      
-      $menu_item5 = 'войти'; $href_item5 = '#5'; //пункт меню
-      $subMenu5_item1 = 'вход'; $subMenu5_href1 = '#5-1'; //пункт подменю
-      $subMenu5_item2 = 'регистрация'; $subMenu5_href2 = '#5-2'; //пункт подменю     
-      
-      //массивы с подменю для каждого пункта меню
-      $subMenuArr2 = [
-        $subMenu2_href1 => '<li class="taskCard__subMenuList">
-        <a href='.$subMenu2_href1.' class="taskCard__subMenuListHref">'.$subMenu2_item1.'</a></li>',
-
-        $subMenu2_href2 => '<li class="taskCard__subMenuList">
-        <a href='.$subMenu2_href2.' class="taskCard__subMenuListHref">'.$subMenu2_item2.'</a></li>'
-      ];
-
-      $subMenuArr3 = [
-        $subMenu3_href1 => '<li class="taskCard__subMenuList">
-        <a href='.$subMenu3_href1.' class="taskCard__subMenuListHref">'.$subMenu3_item1.'</a></li>',
-
-        $subMenu3_href2 => '<li class="taskCard__subMenuList">
-        <a href='.$subMenu3_href2.' class="taskCard__subMenuListHref">'.$subMenu3_item2.'</a></li>'
-      ];
-
-      $subMenuArr4 = [
-        $subMenu4_href1 => '<li class="taskCard__subMenuList">
-        <a href='.$subMenu4_href1.' class="taskCard__subMenuListHref">'.$subMenu4_item1.'</a></li>',
-
-        $subMenu4_href2 => '<li class="taskCard__subMenuList">
-        <a href='.$subMenu4_href2.' class="taskCard__subMenuListHref">'.$subMenu4_item2.'</a></li>'
-      ];
-
-      $subMenuArr5 = [
-        $subMenu5_href1 => '<li class="taskCard__subMenuList">
-        <a href='.$subMenu5_href1.' class="taskCard__subMenuListHref">'.$subMenu5_item1.'</a></li>',
-
-        $subMenu5_href2 => '<li class="taskCard__subMenuList">
-        <a href='.$subMenu5_href2.' class="taskCard__subMenuListHref">'.$subMenu5_item2.'</a></li>'
-      ];
-
-      //массив с пунктами меню и массивами подменю
-      $menuArray = [
-        $href_item1 => ['<li class="taskCard__menuList"><a href='.$href_item1.'>'.$menu_item1.'</a>'],
-        $href_item2 => ['<li class="taskCard__menuList"><a href='.$href_item2.'>'.$menu_item2.'</a>', $subMenuArr2],
-        $href_item3 => ['<li class="taskCard__menuList"><a href='.$href_item3.'>'.$menu_item3.'</a>', $subMenuArr3],
-        $href_item4 => ['<li class="taskCard__menuList"><a href='.$href_item4.'>'.$menu_item4.'</a>', $subMenuArr4],
-        $href_item5 => ['<li class="taskCard__menuList"><a href='.$href_item5.'>'.$menu_item5.'</a>', $subMenuArr5]
-      ];
-
-      //генерация меню на страницу
-      echo '<section class="taskCard taskCard_mainMenu">
-        <ul class="taskCard__menu taskCard__menu_margin">';
-
-      foreach ($menuArray as $menuArray_item) {
-        echo $menuArray_item[0];
-
-        if (count($menuArray_item) > 1) {      
-          echo '<div class="taskCard__subMenuWrap">
-          <ul class="taskCard__subMenu taskCard__subMenu_hidden">';
-
-          foreach ($menuArray_item[1] as $subMenu_item) {
-            echo $subMenu_item;
-          }
-
-          echo '</ul></div>';
-        }
-
-        echo '</li>';
-      }
-
-      echo '</ul></section>';
-      ?>
-      
       <section class="taskCard">
         <h2 class="taskCard__title">Задание 1.</h2>
         <p class="taskCard__text">
