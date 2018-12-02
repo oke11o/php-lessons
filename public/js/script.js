140791ac5e317e12f9b95368d22b9db25d2b001a
DirtyVbif <funkybboy@mail.ru>
2018-12-02 23:44:13 +0300

    modified for new navbar menu 02-12-18

--- public/js/script.js
+++ public/js/script.js
@@ -2,27 +2,6 @@
 function showCode(task) {
   let task1_codeBlock = document.getElementById('task1_codeBlock');
   let task1_codeWrap = document.getElementById('task1_codeWrap');
-
-  let task2_codeBlock = document.getElementById('task2_codeBlock');
-  let task2_codeWrap = document.getElementById('task2_codeWrap');
-
-  let task3_codeBlock = document.getElementById('task3_codeBlock');
-  let task3_codeWrap = document.getElementById('task3_codeWrap');
-
-  let task4_codeBlock = document.getElementById('task4_codeBlock');
-  let task4_codeWrap = document.getElementById('task4_codeWrap');
-
-  let task5_codeBlock = document.getElementById('task5_codeBlock');
-  let task5_codeWrap = document.getElementById('task5_codeWrap');
-
-  let task6_codeBlock = document.getElementById('task6_codeBlock');
-  let task6_codeWrap = document.getElementById('task6_codeWrap');
-
-  let task7_codeBlock = document.getElementById('task7_codeBlock');
-  let task7_codeWrap = document.getElementById('task7_codeWrap');
-
-  let task8_codeBlock = document.getElementById('task8_codeBlock');
-  let task8_codeWrap = document.getElementById('task8_codeWrap');
   
   switch(task) {
     case 'task1':
@@ -30,61 +9,43 @@ function showCode(task) {
       task1_codeWrap.classList.toggle('taskCard__codeWrap_hidden');
       document.location.href = "#task1_location";
       break;
-    case 'task2':
-      task2_codeBlock.classList.toggle('codeBlock_hidden');
-      task2_codeWrap.classList.toggle('taskCard__codeWrap_hidden');
-      document.location.href = "#task2_location";
-      break;
-    case 'task3':
-      task3_codeBlock.classList.toggle('codeBlock_hidden');
-      task3_codeWrap.classList.toggle('taskCard__codeWrap_hidden');
-      document.location.href = "#task3_location";
-      break;
-    case 'task4':
-      task4_codeBlock.classList.toggle('codeBlock_hidden');
-      task4_codeWrap.classList.toggle('taskCard__codeWrap_hidden');
-      document.location.href = "#task4_location";
-      break;
-    case 'task5':
-      task5_codeBlock.classList.toggle('codeBlock_fewHidden');
-      task5_codeWrap.classList.toggle('taskCard__codeWrap_hidden');
-      document.location.href = "#task5_location";
-      break;
-    case 'task6':
-      task6_codeBlock.classList.toggle('codeBlock_hidden');
-      task6_codeWrap.classList.toggle('taskCard__codeWrap_hidden');
-      document.location.href = "#task6_location";
-      break;
-    case 'task7':
-      task7_codeBlock.classList.toggle('codeBlock_hidden');
-      task7_codeWrap.classList.toggle('taskCard__codeWrap_hidden');
-      document.location.href = "#task7_location";
-      break;
-    case 'task8':
-      task8_codeBlock.classList.toggle('codeBlock_hidden');
-      task8_codeWrap.classList.toggle('taskCard__codeWrap_hidden');
-      document.location.href = "#task8_location";
-      break;
   }
 }
 
 //main menu and sub-menu
-window.onload = menuHoverListener;
+window.onload = menuItemsListener;
 
-let menuListsArr = null;
-let menuSubMenuArr = null;
-let subMenuWrapArr = null;
+let menuItemsArr = [];
+let SubMenuArr = [];
+let subMenuWrapArr = [];
+let menuWithSubMenu = [];
 
-function menuHoverListener()
-{  
-  menuListsArr = document.querySelectorAll('.taskCard__menuList'); //5 elements
-  menuSubMenuArr = document.querySelectorAll('.taskCard__subMenu'); //4 elements, starts form second element in menuListsArr[]
-  subMenuWrapArr = document.querySelectorAll('.taskCard__subMenuWrap');
-
-  for (let i = 1; i <= menuSubMenuArr.length; i++) {
-    menuListsArr[i].addEventListener('mouseover', function() { menuSubMenuArr[i-1].classList.remove('taskCard__subMenu_hidden'); });
-    menuListsArr[i].addEventListener('mouseleave', function() { menuSubMenuArr[i-1].classList.add('taskCard__subMenu_hidden'); });
-    subMenuWrapArr[i-1].addEventListener('mouseover', function() { menuSubMenuArr[i-1].classList.remove('taskCard__subMenu_hidden'); });
-    subMenuWrapArr[i-1].addEventListener('mouseleave', function() { menuSubMenuArr[i-1].classList.add('taskCard__subMenu_hidden'); });
+function menuItemsListener()
+{
+  menuItemsArr = document.querySelectorAll('.navBar__item');
+  menuWithSubMenu = document.querySelectorAll('.sub-menu');
+  SubMenuArr = document.querySelectorAll('.subMenu');
+  subMenuWrapArr = document.querySelectorAll('.subMenu__wrap');
+  
+  for (let i = 0; i < menuWithSubMenu.length; i++) {
+    menuWithSubMenu[i].addEventListener('mouseover', function() { SubMenuArr[i].classList.remove('subMenu_hidden'); });
+    menuWithSubMenu[i].addEventListener('mouseleave', function() { SubMenuArr[i].classList.add('subMenu_hidden'); });
+    subMenuWrapArr[i].addEventListener('mouseover', function() { SubMenuArr[i].classList.remove('subMenu_hidden'); });
+    subMenuWrapArr[i].addEventListener('mouseleave', function() { SubMenuArr[i].classList.add('subMenu_hidden'); });
   }
+
+  for (let i = 0; i < menuItemsArr.length; i++) {
+    menuItemsArr[i].addEventListener('click', switchActiveMenuItem);
+  }
+}
+
+function switchActiveMenuItem()
+{
+  for (let i = 0; i < menuItemsArr.length; i++) {
+    if (menuItemsArr[i].classList.contains('navBar__item_active')) {
+      menuItemsArr[i].classList.remove('navBar__item_active')
+    }
+  }
+
+  this.classList.add('navBar__item_active');
 }
