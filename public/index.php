1fa66b2a6c41d75416063618011738c5f3f015f7
DirtyVbif <funkybboy@mail.ru>
2018-12-03 19:31:27 +0300

    add image gallery

--- public/index.php
+++ public/index.php
@@ -12,6 +12,8 @@
   * --- reserved classes list ---
   * .sub-menu
   *   resereved for js to initialize menu with dropdown sub-menu
+  * .spawn-image
+  *   reserved for js to initialize all spawned images
   */
   error_reporting(E_ALL);
   ?>
@@ -31,220 +33,21 @@
     </header>
 
     <main class="bodyWrap__main">
-      <section class="taskCard">
-        <h2 class="taskCard__title">Задание 1.</h2>
-        <p class="taskCard__text">
-          С помощью цикла while вывести все числа в промежутке от 0 до 100, которые делятся на 3 без остатка.
-        </p>
-
-        <?php
-        $num = 0;
-
-        echo "<code>";
-        while($num < 100) {
-          if ($num % 3 == 0 && $num != 0) {
-            echo "$num, ";
-          }
-          $num++;
-        }
-        echo "</code>";
-        ?>
+      <section class="popup__wrap" id="popup-slider">
 
       </section>
-
       <section class="taskCard">
-        <h2 class="taskCard__title">Задание 2.</h2>
-        <p class="taskCard__text">
-        С помощью цикла do…while написать функцию для вывода чисел от 0 до 10, чтобы результат выглядел так:
-        0 – это ноль. <br>
-        1 – нечетное число. <br>
-        2 – четное число. <br>
-        3 – нечетное число. <br>
-        … <br>
-        10 – четное число.
-        </p>
+        <h2 class="taskCard__title">Галлерея.</h2>
 
-        <?php
-        $num2 = 0;
-        
-        echo "<code>";
+        <div class="taskCard__gallery">
+          <?php
+          require '../core/images-spawn.php';
 
-        do {
-          if ($num2 == 0) {
-            echo "$num2 - ноль. <br>";
-          } elseif ($num2 % 2 == 0) {
-            echo "$num2 - чётное число. <br>";
-          } else {
-            echo "$num2 - нечётное число. <br>";
-          }
+          $galleryDir = 'img/gallery';
 
-          $num2++;
-        } while ($num2 >= 0 && $num2 <= 10);
-
-        echo "</code>";
-        ?>
-
-      </section>
-
-      <section class="taskCard">
-        <h2 class="taskCard__title">Задание 3.</h2>
-        <p class="taskCard__text">
-        Объявить массив, в котором в качестве ключей будут использоваться названия областей, а в качестве значений – массивы с названиями городов из соответствующей области. Вывести в цикле значения массива, чтобы результат был таким:
-        Московская область:
-        Москва, Зеленоград, Клин
-        Ленинградская область:
-        Санкт-Петербург, Всеволожск, Павловск, Кронштадт
-        Рязанская область …
-        </p>
-
-        <?php
-        $citiesArray = [
-          'Московская Область' => ['Москва', 'Зеленоград', 'Клин', 'Химки'],
-          'Ленинградская Область' => ['Санкт-Петербург', 'Всеволжск', 'Павловск', 'Кронштадт'],
-          'Рязанская Область' => ['Рязань', 'Сараи', 'Шилово', 'Сасово']
-        ];
-        
-        echo "<code>";
-
-        foreach ($citiesArray as $key => $value) {
-          echo $key.":<br>";
-          foreach ($citiesArray[$key] as $value) {
-            echo " - ".$value.".<br>";
-          }
-        }
-
-        echo "</code>";
-        ?>
-
-      </section>
-
-      <section class="taskCard">
-        <h2 class="taskCard__title">Задание 4.</h2>
-        <p class="taskCard__text">
-          Объявить массив, индексами которого являются буквы русского языка, а значениями – соответствующие латинские буквосочетания (‘а’=> ’a’, ‘б’ => ‘b’, ‘в’ => ‘v’, ‘г’ => ‘g’, …, ‘э’ => ‘e’, ‘ю’ => ‘yu’, ‘я’ => ‘ya’). <br>
-          Написать функцию транслитерации строк.
-        </p>
-
-        <?php
-        $testStringText = 'Тестовая строка для задания.';
-
-        function translitString($stringToTranslit)
-        {
-          mb_strtolower($stringToTranslit);
-
-          $lettersArray = [
-            'а' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd', 'е' => 'e', 'ё' => 'yo', 'ж' => 'j',
-            'з' => 'z', 'и' => 'i', 'й' => 'yi', 'к' => 'k', 'л' => 'l', 'м' => 'm', 'н' => 'n', 'о' => 'o',
-            'п' => 'p', 'р' => 'r', 'с' => 's', 'т' => 't', 'у' => 'y', 'ф' => 'f', 'х' => 'h', 'ц' => 'c',
-            'ч' => 'ch','ш' => 'sh', 'щ' => 'sch', 'ъ' => 'j', 'ы' => 'i', 'ь' => "'", 'э' => 'e', 'ю' => 'u', 'я' => 'ya'
-          ];
-          $stringToTranslit = strtr($stringToTranslit, $lettersArray);
-
-          return $stringToTranslit;
-        }
-
-        echo "<code>";
-        echo translitString($testStringText);
-        echo "</code>";
-        ?>
-
-      </section>
-
-      <section class="taskCard">
-        <h2 class="taskCard__title">Задание 5.</h2>
-        <p class="taskCard__text">
-          Написать функцию, которая заменяет в строке пробелы на подчеркивания и возвращает видоизмененную строчку.
-        </p>
-
-        <?php
-        function convertSpaces($stringWithSpaces)
-        {
-          $stringWithSpaces = strtr($stringWithSpaces, [" " => "_"]);
-
-          return $stringWithSpaces;
-        }
-
-        echo "<code>";
-        echo convertSpaces($testStringText);
-        echo "</code>";
-        ?>
-
-      </section>
-
-      <section class="taskCard">
-        <h2 class="taskCard__title">Задание 6.</h2>
-        <p class="taskCard__text">
-          В имеющемся шаблоне сайта заменить статичное меню (ul - li) на генерируемое через PHP. Необходимо представить пункты меню как элементы массива и вывести их циклом. Подумать, как можно реализовать меню с вложенными подменю? Попробовать его реализовать.
-        </p>
-
-        <!-- меню выполнено в первом <section> в <main> -->
-
-      </section>
-
-      <section class="taskCard">
-        <h2 class="taskCard__title">Задание 7.</h2>
-        <p class="taskCard__text">
-        *Вывести с помощью цикла for числа от 0 до 9, НЕ используя тело цикла. То есть выглядеть должно так: <br>
-        <code>for (…){ // здесь пусто}</code>
-        </p>
-
-        <?php
-        for ($i = 0; $i < 10; print_r($i++.", ")) { /*здесь пусто*/ }
-        ?>
-
-      </section>
-
-      <section class="taskCard">
-        <h2 class="taskCard__title">Задание 8.</h2>
-        <p class="taskCard__text">
-          *Повторить третье задание, но вывести на экран только города, начинающиеся с буквы «К».
-        </p>
-
-        <?php
-        echo "<code>";
-
-        foreach ($citiesArray as $key => $value) {
-          foreach ($citiesArray[$key] as $value) {
-            if (mb_substr($value, 0, 1,"utf-8") == 'К') {
-              echo $value . '</br>';
-            }
-          }
-        }
-
-        echo "</code>";
-        ?>
-
-      </section>
-
-      <section class="taskCard">
-        <h2 class="taskCard__title">Задание 9.</h2>
-        <p class="taskCard__text">
-          *Объединить две ранее написанные функции в одну, которая получает строку на русском языке, производит транслитерацию и замену пробелов на подчеркивания (аналогичная задача решается при конструировании url-адресов на основе названия статьи в блогах).
-        </p>
-
-        <?php
-        echo "вариант 1.";
-        echo "<br>";
-        echo "<code>";
-        echo convertSpaces(translitString($testStringText));
-        echo "</code>";
-
-        function trnsTxtWithSpaces($txtWithSpacesToTrns)
-        {
-          $txtWithSpacesToTrns = translitString($txtWithSpacesToTrns);
-          $txtWithSpacesToTrns = convertSpaces($txtWithSpacesToTrns);
-
-          return $txtWithSpacesToTrns;
-        }
-
-        echo "<br>";
-        echo "вариант 2.";
-        echo "<br>";
-        echo "<code>";
-        echo trnsTxtWithSpaces($testStringText);
-        echo "</code>";
-        ?>
-
+          spawnImages($galleryDir);
+          ?>
+        </div>
       </section>
     </main>
     
