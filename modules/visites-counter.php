59020520234c32cd4cfc6c00b8b8a1d33269806e
DirtyVbif <funkybboy@mail.ru>
2018-12-05 01:12:15 +0300

    modified counter function

--- modules/visites-counter.php
+++ modules/visites-counter.php
@@ -1,19 +1,14 @@
 <?php
-
 function counter($filename)
 {
   $file = fopen('../resources/'.$filename, 'c');
   $counter = (int)file_get_contents('../resources/'.$filename);
 
-  if (!$counter) {
-    $counter = 1;
-    fwrite($file, $counter);
-    fclose($file);
-  } else {
-    $counter++;
-    fwrite($file, $counter);
-    fclose($file);
-  }
+  $counter++;
+
+  file_put_contents('../resources/'.$filename, $counter);
+
+  fclose($file);
   
   return $counter;
 }
