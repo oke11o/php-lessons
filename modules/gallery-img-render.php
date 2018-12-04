e12cdd42f2b677123954e0577e770442bce53eba
DirtyVbif <funkybboy@mail.ru>
2018-12-05 01:04:42 +0300

    add function to make string with name from file name

--- modules/gallery-img-render.php
+++ modules/gallery-img-render.php
@@ -1,42 +1,50 @@
 <?php
 function renderGalleryImages($imgDir)
 {
-  //include function to get array of images
-  include '../core/get-files-array.php';
-
   //function to get array of images
   $imgArray = initFilesArray($imgDir);
 
   //allowed file types for gallery
   $galImgTypes = ['jpg', 'jpeg', 'png', 'gif'];
-  $trSymbols = ["-0" => " ", "-" => " ", "_" => " "];
+  $trSymbols = ["-0" => " ", "-" => " ", "_" => " ", " 0" => " "];
 
   $i = 0;
 
   foreach($imgArray as $img) {
     $img_parts = explode('.', $img);
     $curImgExt = strtolower(array_pop($img_parts));
-    $curImgName = strtolower(array_shift($img_parts));
-
-    //if space as first symbol then delete space
-    if (array_key_exists(substr($curImgName, 0, 1), $trSymbols)) {
-      $curImgName = ucfirst(substr($curImgName, 1));
-    } else {
-      $curImgName = ucfirst($curImgName);
-    }
-
-    $curImgName = strtr($curImgName, $trSymbols);
 
     //if img type incorrect or it isn't a file then continue to next step of foreach;
     if ((!in_array($curImgExt, $galImgTypes)) || ($img == '.') || ($img == '..')) {
       continue;
     }
 
-    echo('<div class="taskCard__imgWrap spawn-image">');
-    echo('<img src="'.$imgDir.'/'.$img.'" alt="'.$i.'" class="taskCard__img">');
-    echo('<span>'.$curImgName.'</span>');
-    echo('</div>');
+    $divElementOpen = '<div class="taskCard__imgWrap spawn-image">';
+    $imgElement = '<img src="'.$imgDir.'/'.$img.'" alt="'.$i.'" class="taskCard__img">';
+    $imgElementText = '<span>'.setFileName($img_parts, $trSymbols).'</span>';
+    $divElementClose = '</div>';
+
+    echo($divElementOpen.$imgElement.$imgElementText.$divElementClose);
 
     $i++;
   }
+}
+
+// function to translate file name to strin.
+// Required array of name parts without extention
+// Required array of unwanted symbols
+function setFileName($nameArray, $symbolsArray)
+{
+  $currentName = strtolower(implode(' ', $nameArray));
+
+  //if symbol from array as first symbol of the name then delete that symbol
+  if (array_key_exists(substr($currentName, 0, 1), $symbolsArray)) {
+    $currentName = ucfirst(substr($currentName, 1));
+  } else {
+    $currentName = ucfirst($currentName);
+  }
+
+  $currentName = strtr($currentName, $symbolsArray);
+
+  return $currentName;
 }
