fb75d7af71b1880c6d7a34b7d0ef55d8d29988bd
DirtyVbif <funkybboy@mail.ru>
2018-12-04 20:32:12 +0300

    modified function for rendering images in gallery

--- modules/gallery-img-render.php
+++ modules/gallery-img-render.php
@@ -9,6 +9,8 @@ function renderGalleryImages($imgDir)
 
   //allowed file types for gallery
   $galImgTypes = ['jpg', 'jpeg', 'png', 'gif'];
+  $trSymbolsFrom = ["-0", "-", "_"];
+  $trSymbols = ["-0" => " ", "-" => " ", "_" => " "];
 
   $i = 0;
 
@@ -16,6 +18,15 @@ function renderGalleryImages($imgDir)
     $img_parts = explode('.', $img);
     $curImgExt = strtolower(array_pop($img_parts));
     $curImgName = strtolower(array_shift($img_parts));
+
+    //if space as first symbol then delete space
+    if (in_array(substr($curImgName, 0, 1), $trSymbolsFrom)) {
+      $curImgName = ucfirst(substr($curImgName, 1));
+    } else {
+      $curImgName = ucfirst($curImgName);
+    }
+
+    $curImgName = strtr($curImgName, $trSymbols);
 
     //if img type incorrect or it isn't a file then continue to next step of foreach;
     if ((!in_array($curImgExt, $galImgTypes)) || ($img == '.') || ($img == '..')) {
