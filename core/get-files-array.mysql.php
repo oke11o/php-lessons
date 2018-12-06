2390a0075d30bd1898ed6f42a6c8ac12fcb58fd5
DirtyVbif <funkybboy@mail.ru>
2018-12-07 00:16:06 +0300

    add keys in returned array from that function

--- core/get-files-array.mysql.php
+++ core/get-files-array.mysql.php
@@ -3,6 +3,9 @@ function getArrayFromMySQL($connection, $table_name, $name)
 {
   $names = [];
   $src = [];
+  $clicks = [];
+  $id = [];
+
   $array = mysqli_query($connection, "SELECT * FROM ".$table_name." WHERE name LIKE ".$name);
   
   foreach ($array as $arrayObj) {
@@ -11,12 +14,25 @@ function getArrayFromMySQL($connection, $table_name, $name)
     } else {
       $names['no data'] = 'No name data in database.';
     }
-    if (array_key_exists('name', $arrayObj)) {
+
+    if (array_key_exists('src', $arrayObj)) {
       $src[] = $arrayObj['src'];
     } else {
       $src['no data'] = 'No path data in database.';
     }
+
+    if (array_key_exists('views', $arrayObj)) {
+      $clicks[] = $arrayObj['views'];
+    } else {
+      $clicks['no data'] = 'No path data in database.';
+    }
+
+    if (array_key_exists('id', $arrayObj)) {
+      $id[] = $arrayObj['id'];
+    } else {
+      $id['no data'] = 'No path data in database.';
+    }
   }
   
-  return ['names' => $names, 'src' => $src];
+  return ['id' => $id, 'names' => $names, 'src' => $src, 'views' => $clicks];
 }
