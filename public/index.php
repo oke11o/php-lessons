5d1487800935d520a2cc16fa9b032b56596cc738
DirtyVbif <funkybboy@mail.ru>
2018-12-06 21:44:52 +0300

    pass tests with rendering images from database

--- public/index.php
+++ public/index.php
@@ -21,7 +21,7 @@
   * init.php includes:
   *   $mysqli //connection to database
   */
-  require_once '../core/init.php';
+  require '../core/init.php';
   ?>
 </head>
 
@@ -72,11 +72,12 @@
 
         <div class="taskCard__gallery">
           <?php
-          include '../core/get-files-array.mysql.php';
-
           //'names' => ['name 1', ... 'name n'], 'src' => ['path 1', ... 'path n']
           $images_house1_array = getArrayFromMySQL($mysqli, '`lesson_05`', '"House 1%"');
-          
+
+          include '../modules/gallery-image-render.php';
+
+          renderImagesFromMySQL($images_house1_array);
           ?>
         </div>
       </section>
