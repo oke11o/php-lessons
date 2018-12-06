13d526a31755518f2de4dd008e66065cd96d8925
DirtyVbif <funkybboy@mail.ru>
2018-12-06 21:45:28 +0300

    add module for rendering images to gallery

---
+++ modules/gallery-image-render.php
@@ -0,0 +1,14 @@
+<?php
+function renderImagesFromMySQL($array)
+{
+  $arrayLength = count($array['src']);
+  
+  for ($i = 0; $i < $arrayLength; $i++) {
+    $divElementOpen = '<div class="taskCard__imgWrap spawn-image">';
+    $imgElement = '<img src="'.$array['src'][$i].'" alt="'.$i.'" class="taskCard__img">';
+    $imgElementText = '<span>'.$array['names'][$i].'</span>';
+    $divElementClose = '</div>';
+
+    echo($divElementOpen.$imgElement.$imgElementText.$divElementClose);
+  }
+}
