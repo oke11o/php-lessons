6deb5e7da11d0adfb7ee23f05ee339d5ca6afc1c
DirtyVbif <funkybboy@mail.ru>
2018-12-06 21:45:08 +0300

    add function to get array of files from database

--- core/get-files-array.mysql.php
+++ core/get-files-array.mysql.php
@@ -6,9 +6,17 @@ function getArrayFromMySQL($connection, $table_name, $name)
   $array = mysqli_query($connection, "SELECT * FROM ".$table_name." WHERE name LIKE ".$name);
   
   foreach ($array as $arrayObj) {
-    $names[] = $arrayObj['name'];
-    $src[] = $arrayObj['src'];
+    if (array_key_exists('name', $arrayObj)) {
+      $names[] = $arrayObj['name'];
+    } else {
+      $names['no data'] = 'No name data in database.';
+    }
+    if (array_key_exists('name', $arrayObj)) {
+      $src[] = $arrayObj['src'];
+    } else {
+      $src['no data'] = 'No path data in database.';
+    }
   }
-
+  
   return ['names' => $names, 'src' => $src];
 }
