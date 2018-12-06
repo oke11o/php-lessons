7b58aa78c2037cda9931aa1832501919d65f12a6
DirtyVbif <funkybboy@mail.ru>
2018-12-06 21:07:10 +0300

    modified for working with database

---
+++ core/init.php
@@ -0,0 +1,16 @@
+<?php
+//include all modules and files here
+require_once '../core/get-files-array.php'; //include function to get array of files
+include_once '../core/main-date.php';
+
+include_once '../modules/gallery-img-render.php'; //include function to render images from array
+include_once '../modules/visites-counter.php';
+
+//connection to database
+$mysql_config = include_once '../config/config.mysql.php';
+
+$mysqli = mysqli_connect($mysql_config['db-localhost'],
+  $mysql_config['db-user'],
+  $mysql_config['db-password'],
+  $mysql_config['db-database']
+);
