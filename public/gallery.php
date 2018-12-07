16821364d733562a031ba6a1a0ae740acee95cbb
DirtyVbif <funkybboy@mail.ru>
2018-12-07 17:34:14 +0300

    add comments

--- public/gallery.php
+++ public/gallery.php
@@ -2,13 +2,16 @@
 
 <div class="taskCard__gallery">
   <?php
+  //include rendering function of image on this page
   include '../modules/gallery-image-render.php';
 
   //'names' => ['name 1', ... 'name n'], 'src' => ['path 1', ... 'path n']
   $images_house2_array = getArrayFromMySQL($mysqli, '`lesson_05`', '"House 2%"');
 
+  //call rendering function of images on this page
   renderImagesFromMySQL($images_house2_array); 
 
+  //call function to count number of views for this gallery
   $house2_array_length = count($images_house2_array['id']);
 
   for ($house2_i = (int)$images_house2_array['id'][0]; $house2_i <= ($house2_array_length + $images_house2_array['id'][0]); $house2_i++) {
