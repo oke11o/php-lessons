15ae607777c5da2aefa25e0e92ebb4291cbedebc
DirtyVbif <funkybboy@mail.ru>
2018-12-07 17:29:47 +0300

    added call of function to count number of views

--- public/gallery.php
+++ public/gallery.php
@@ -2,11 +2,17 @@
 
 <div class="taskCard__gallery">
   <?php
+  include '../modules/gallery-image-render.php';
+
   //'names' => ['name 1', ... 'name n'], 'src' => ['path 1', ... 'path n']
   $images_house2_array = getArrayFromMySQL($mysqli, '`lesson_05`', '"House 2%"');
 
-  include '../modules/gallery-image-render.php';
+  renderImagesFromMySQL($images_house2_array); 
 
-  renderImagesFromMySQL($images_house2_array);
+  $house2_array_length = count($images_house2_array['id']);
+
+  for ($house2_i = (int)$images_house2_array['id'][0]; $house2_i <= ($house2_array_length + $images_house2_array['id'][0]); $house2_i++) {
+    clickCounter($mysqli, $house2_i);
+  }
   ?>
 </div>
