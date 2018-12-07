47df46ab4a1dba41366b70086d98f8b76b4764d5
DirtyVbif <funkybboy@mail.ru>
2018-12-07 17:29:30 +0300

    added hidden line with number of views

--- modules/gallery-image-render.php
+++ modules/gallery-image-render.php
@@ -5,10 +5,11 @@ function renderImagesFromMySQL($array)
   
   for ($i = 0; $i < $arrayLength; $i++) {
     $divElementOpen = '<div class="taskCard__imgWrap spawn-image">';
-    $imgElement = '<img src="'.$array['src'][$i].'" alt="'.$i.'" class="taskCard__img" data-number="'.$array['id'][$i].'">';
+    $imgElement = '<img src="'.$array['src'][$i].'" alt="'.$i.'" class="taskCard__img" data-number="'.$array['id'][$i].'">';    
+    $divViews = '<span class="image-views">'.$array['views'][$i].'</span>';
     $imgElementText = '<span>'.$array['names'][$i].'</span>';
     $divElementClose = '</div>';
 
-    echo($divElementOpen.$imgElement.$imgElementText.$divElementClose);
+    echo($divElementOpen.$imgElement.$divViews.$imgElementText.$divElementClose);
   }
 }
