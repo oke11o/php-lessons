6cc0d5e163acebaafc2ac9a1f2736ac344a49a56
DirtyVbif <funkybboy@mail.ru>
2018-12-07 17:29:11 +0300

    modified for number of gallery views

--- core/click-counter.php
+++ core/click-counter.php
@@ -1,13 +1,13 @@
 <?php
-function clickCounter($id)
+function clickCounter($connection, $id)
 {
-  $views = mysqli_query($mysqli, 'SELECT `views` FROM `lesson_05` WHERE `id` = '.$id.';');
+  $views = mysqli_query($connection, 'SELECT `views` FROM `lesson_05` WHERE `id` = '.$id.';');
 
   foreach ($views as $view) {
     $numOfView = (int)$view['views'];
   }
 
   $numOfView++;
-
-  mysqli_query($mysqli, 'UPDATE `lesson_05` SET `views` = \'$numOfView\' WHERE `lesson_05`.`id` = '.$id.';');
+  
+  mysqli_query($connection, 'UPDATE `lesson_05` SET `views` = "'.$numOfView.'" WHERE `lesson_05`.`id` = '.$id.';');
 }
