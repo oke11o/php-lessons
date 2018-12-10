bb401b2850de81aa468fcc4f67b919140e7f4a46
DirtyVbif <funkybboy@mail.ru>
2018-12-10 16:36:28 +0300

    update links

--- modules/nav-bar-menu.php
+++ modules/nav-bar-menu.php
@@ -2,23 +2,29 @@
 $navBarMenuArray = [
   'main' => [
     'name' => 'главная',
-    'href' => '/'
+    'href' => '?page=main'
   ],
   'gallery' => [
     'name' => 'галлерея',
-    'href' => '?page=gallery'
+    'href' => '?page=gal1',
+    'subMenu' => [
+      [
+        'name' => 'House&nbsp;type&nbsp;1',
+        'href' => '?page=gal1',
+      ],
+      [
+        'name' => 'House&nbsp;type&nbsp;2',
+        'href' => '?page=gal2',
+      ]
+    ]
   ],
   'contacts' => [
     'name' => 'контакты',
     'href' => '#cont',
     'subMenu' => [
-      // [
-      //   'name' => 'о&nbsp;нас',
-      //   'href' => '#',
-      // ],
       [
-        'name' => 'урок-06',
-        'href' => '?page=lesson',
+        'name' => 'о&nbsp;нас',
+        'href' => '#',
       ],
       [
         'name' => 'обратная&nbsp;связь',
