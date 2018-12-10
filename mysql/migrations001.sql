b04f803257ee82a52287c4f0e919a6e784e9921a
DirtyVbif <funkybboy@mail.ru>
2018-12-10 16:36:11 +0300

    update

--- mysql/migrations001.sql
+++ mysql/migrations001.sql
@@ -19,11 +19,11 @@ VALUES
   ('House 1 preview 5', 'img/gallery/house-01-preview-05.jpg'),
   ('House 1 preview 6', 'img/gallery/house-01-preview-06.jpg'),
   ('House 1 preview 7', 'img/gallery/house-01-preview-07.jpg'),
-  ('House 1 constructions 1', 'img/gallery/house-01-const-01.jpg'),
-  ('House 1 constructions 2', 'img/gallery/house-01-const-02.jpg'),
-  ('House 1 constructions 3', 'img/gallery/house-01-const-03.jpg'),
-  ('House 1 constructions 4', 'img/gallery/house-01-const-04.jpg'),
-  ('House 1 constructions 5', 'img/gallery/house-01-const-05.jpg');
+  ('House 1 constructions%nbsp;1', 'img/gallery/house-01-const-01.jpg'),
+  ('House 1 constructions%nbsp;2', 'img/gallery/house-01-const-02.jpg'),
+  ('House 1 constructions%nbsp;3', 'img/gallery/house-01-const-03.jpg'),
+  ('House 1 constructions%nbsp;4', 'img/gallery/house-01-const-04.jpg'),
+  ('House 1 constructions%nbsp;5', 'img/gallery/house-01-const-05.jpg');
 
 INSERT INTO lesson_05 (name, src)
 VALUES
@@ -37,8 +37,20 @@ VALUES
   ('House 2 preview 8', 'img/gallery/house-02-preview-08.jpg'),
   ('House 2 preview 9', 'img/gallery/house-02-preview-09.jpg'),
   ('House 2 preview 10', 'img/gallery/house-02-preview-10.jpg'),
-  ('House 2 constructions 1', 'img/gallery/house-02-const-04.jpg'),
-  ('House 2 constructions 2', 'img/gallery/house-02-const-03.jpg'),
-  ('House 2 constructions 3', 'img/gallery/house-02-const-02.jpg'),
-  ('House 2 constructions 4', 'img/gallery/house-02-const-01.jpg'),
-  ('House 2 constructions 5', 'img/gallery/house-02-const-05.jpg');
+  ('House 2 constructions%nbsp;1', 'img/gallery/house-02-const-04.jpg'),
+  ('House 2 constructions%nbsp;2', 'img/gallery/house-02-const-03.jpg'),
+  ('House 2 constructions%nbsp;3', 'img/gallery/house-02-const-02.jpg'),
+  ('House 2 constructions%nbsp;4', 'img/gallery/house-02-const-01.jpg'),
+  ('House 2 constructions%nbsp;5', 'img/gallery/house-02-const-05.jpg');
+
+-- UPDATE `lesson_05` SET `name` = 'House 1 constructions&nbsp;1' WHERE `lesson_05`.`id` = 8;
+-- UPDATE `lesson_05` SET `name` = 'House 1 constructions&nbsp;2' WHERE `lesson_05`.`id` = 9;
+-- UPDATE `lesson_05` SET `name` = 'House 1 constructions&nbsp;3' WHERE `lesson_05`.`id` = 10;
+-- UPDATE `lesson_05` SET `name` = 'House 1 constructions&nbsp;4' WHERE `lesson_05`.`id` = 11;
+-- UPDATE `lesson_05` SET `name` = 'House 1 constructions&nbsp;5' WHERE `lesson_05`.`id` = 12;
+
+-- UPDATE `lesson_05` SET `name` = 'House 2 constructions&nbsp;1' WHERE `lesson_05`.`id` = 24;
+-- UPDATE `lesson_05` SET `name` = 'House 2 constructions&nbsp;2' WHERE `lesson_05`.`id` = 25;
+-- UPDATE `lesson_05` SET `name` = 'House 2 constructions&nbsp;3' WHERE `lesson_05`.`id` = 26;
+-- UPDATE `lesson_05` SET `name` = 'House 2 constructions&nbsp;4' WHERE `lesson_05`.`id` = 27;
+-- UPDATE `lesson_05` SET `name` = 'House 2 constructions&nbsp;5' WHERE `lesson_05`.`id` = 28;
