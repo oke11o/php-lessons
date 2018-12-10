c3f279f932aa465d14a1553244d32055bca3f17e
DirtyVbif <funkybboy@mail.ru>
2018-12-10 22:11:20 +0300

    add description field to function

--- core/get-files-array.mysql.php
+++ core/get-files-array.mysql.php
@@ -1,38 +1,34 @@
 <?php
-function getArrayFromMySQL($connection, $table_name, $name)
+function getArrayFromMySQL($connection, $table_name, $column, $name)
 {
   $names = [];
   $src = [];
-  $clicks = [];
+  $views = [];
   $id = [];
 
-  $array = mysqli_query($connection, "SELECT * FROM ".$table_name." WHERE name LIKE ".$name);
+  $array = mysqli_query($connection, "SELECT * FROM ".$table_name." WHERE ".$column." LIKE ".$name);
   
-  foreach ($array as $arrayObj) {
+  foreach ($array as $arrayObj) {   
+    if (array_key_exists('id', $arrayObj)) {
+      $id[] = $arrayObj['id'];
+    }
+
     if (array_key_exists('name', $arrayObj)) {
       $names[] = $arrayObj['name'];
-    } else {
-      $names['no data'] = 'No name data in database.';
     }
 
     if (array_key_exists('src', $arrayObj)) {
       $src[] = $arrayObj['src'];
-    } else {
-      $src['no data'] = 'No path data in database.';
     }
 
     if (array_key_exists('views', $arrayObj)) {
-      $clicks[] = $arrayObj['views'];
-    } else {
-      $clicks['no data'] = 'No path data in database.';
+      $views[] = $arrayObj['views'];
     }
 
-    if (array_key_exists('id', $arrayObj)) {
-      $id[] = $arrayObj['id'];
-    } else {
-      $id['no data'] = 'No path data in database.';
-    }
+    if (array_key_exists('description', $arrayObj)) {
+      $description[] = $arrayObj['description'];
+    } 
   }
   
-  return ['id' => $id, 'names' => $names, 'src' => $src, 'views' => $clicks];
+  return ['id' => $id, 'names' => $names, 'src' => $src, 'views' => $views, 'description' => $description];
 }
