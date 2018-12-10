e4dc591e0c06aebca850e01173693eab0d938cb6
DirtyVbif <funkybboy@mail.ru>
2018-12-10 22:11:00 +0300

    update html-elements inside

--- modules/gallery-image-render.php
+++ modules/gallery-image-render.php
@@ -4,11 +4,11 @@ function renderImagesFromMySQL($array)
   $arrayLength = count($array['src']);
   
   for ($i = 0; $i < $arrayLength; $i++) {
-    $divElementOpen = '<div class="taskCard__imgWrap spawn-image">';
+    $divElementOpen = '<a href="?page=product&product_id='.$array['id'][$i].'" class="taskCard__imgWrap spawn-image">';
     $imgElement = '<img src="'.$array['src'][$i].'" alt="'.$i.'" class="taskCard__img" data-number="'.$array['id'][$i].'">';    
     $divViews = '<span class="image-views">'.$array['views'][$i].'</span>';
     $imgElementText = '<span>'.$array['names'][$i].'</span>';
-    $divElementClose = '</div>';
+    $divElementClose = '</a>';
 
     echo($divElementOpen.$imgElement.$divViews.$imgElementText.$divElementClose);
   }
