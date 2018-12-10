3942e504755409382eec46f8e3f90a9b1fdb9ff1
DirtyVbif <funkybboy@mail.ru>
2018-12-10 22:10:38 +0300

    update for modified functions

--- public/content/gallery-house1.php
+++ public/content/gallery-house1.php
@@ -1,22 +1,25 @@
-<?php include '../modules/gallery-nav.php'; ?>
+<?php
+//add navigations button for other galleries
+include '../modules/gallery-nav.php';
+?>
 
 <section class="taskCard taskCard_width">
-  <h2 class="taskCard__title">House type 1.</h2>
+  <h2 class="taskCard__title">House type 1</h2>
 
   <div class="taskCard__gallery">
     <?php
     //'names' => ['name 1', ... 'name n'], 'src' => ['path 1', ... 'path n']
-    $images_house1_array = getArrayFromMySQL($mysqli, '`lesson_05`', '"House 1%"');
+    $images_house1_array = getArrayFromMySQL($mysqli, '`lesson_05`', '`type`', '"house1"');
 
     //call rendering function of images on this page
     renderImagesFromMySQL($images_house1_array);
 
     //call function to count number of views for this gallery
-    $house1_array_length = count($images_house1_array['id']);
+    // $house1_array_length = count($images_house1_array['id']);
 
-    for ($house1_i = (int)$images_house1_array['id'][0]; $house1_i <= $house1_array_length; $house1_i++) {
-      clickCounter($mysqli, $house1_i);
-    }
+    // for ($house1_i = (int)$images_house1_array['id'][0]; $house1_i <= $house1_array_length; $house1_i++) {
+    //   clickCounter($mysqli, $house1_i);
+    // }
     ?>
   </div>  
 </section>
--- public/content/gallery-house2.php
+++ public/content/gallery-house2.php
@@ -1,22 +1,25 @@
-<?php include '../modules/gallery-nav.php'; ?>
+<?php
+//add navigations button for other galleries
+include '../modules/gallery-nav.php';
+?>
 
 <section class="taskCard taskCard_width">
-  <h2 class="taskCard__title">House type 2.</h2>
+  <h2 class="taskCard__title">House type 2</h2>
 
   <div class="taskCard__gallery">
     <?php
     //'names' => ['name 1', ... 'name n'], 'src' => ['path 1', ... 'path n']
-    $images_house2_array = getArrayFromMySQL($mysqli, '`lesson_05`', '"House 2%"');
+    $images_house2_array = getArrayFromMySQL($mysqli, '`lesson_05`', '`type`', '"house2"');
 
     //call rendering function of images on this page
     renderImagesFromMySQL($images_house2_array); 
 
     //call function to count number of views for this gallery
-    $house2_array_length = count($images_house2_array['id']);
+    // $house2_array_length = count($images_house2_array['id']);
 
-    for ($house2_i = (int)$images_house2_array['id'][0]; $house2_i <= ($house2_array_length + $images_house2_array['id'][0]); $house2_i++) {
-      clickCounter($mysqli, $house2_i);
-    }
+    // for ($house2_i = (int)$images_house2_array['id'][0]; $house2_i <= ($house2_array_length + $images_house2_array['id'][0]); $house2_i++) {
+    //   clickCounter($mysqli, $house2_i);
+    // }
     ?>
   </div>
 </section>
