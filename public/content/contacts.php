af6ed783b0e89d050f162a303e70cc3cac85355c
DirtyVbif <funkybboy@mail.ru>
2018-12-10 22:10:13 +0300

    add new pages for site

---
+++ public/content/contacts.php
@@ -0,0 +1,9 @@
+<section class="taskCard taskCard_width">
+  <h2 class="taskCard__title">Контакты</h2>
+
+  <div class="taskCard__gallery">
+    <?php
+
+    ?>
+  </div>  
+</section>
---
+++ public/content/feedback.php
@@ -0,0 +1,9 @@
+<section class="taskCard taskCard_width">
+  <h2 class="taskCard__title">Отзывы</h2>
+
+  <div class="taskCard__gallery">
+    <?php
+
+    ?>
+  </div>  
+</section>
