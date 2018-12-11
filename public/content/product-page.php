a0955a00648731dff4b54a73bb0fe29fd5074ed5
DirtyVbif <funkybboy@mail.ru>
2018-12-11 22:50:47 +0300

    add product image and description for product

--- public/content/product-page.php
+++ public/content/product-page.php
@@ -11,9 +11,8 @@ $productArray = getArrayFromMySQL($mysqli, "SELECT * FROM `gallery` WHERE `id` =
 <section class="taskCard taskCard_width">
   <h2 class="taskCard__title"><?=$productArray['names'][0]?></h2>
 
-  <div class="taskCard__gallery">
-    <?php
-    
-    ?>
+  <div class="product">
+    <img src="<?=$productArray['src'][0]?>" alt="<?=$productArray['type'][0]?>">
+    <p><?=$productArray['description'][0]?></p>
   </div>  
 </section>
