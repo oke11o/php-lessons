80999ac8386ec9deec51f2c7753fe18901e0a7ee
DirtyVbif <funkybboy@mail.ru>
2018-12-11 16:48:35 +0300

    restructure

--- core/init.php
+++ core/init.php
@@ -13,7 +13,7 @@ require_once 'get-files-array.mysql.php';
 include_once 'main-date.php';
 include_once 'math-functions.php';
 include_once 'visites-counter.php'; //functions to count visites of current page
-include_once 'feedback.mysql.php'; //functions to store feedback in database
+include_once 'feedback-create.php';
 
 include '../modules/gallery-image-render.php'; //rendering function for images
 include '../modules/show-feedbacks.php'; //function to show feedbacks from array
