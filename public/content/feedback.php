9d052355b61bd7dfdb9490d457d4d15057ae3d61
DirtyVbif <funkybboy@mail.ru>
2018-12-11 16:48:14 +0300

    moved function to the top of index.php

--- public/content/feedback.php
+++ public/content/feedback.php
@@ -14,7 +14,7 @@
 
     <div class="feedback__section">
       <label for="feedback_text">Текст сообщения: <code>*</code></label>
-      <textarea name="feedback_text" class="feedback__text" placeholder="Введите ваше сообщение"></textarea>
+      <textarea name="feedback_text" class="feedback__text" placeholder="Введите ваше сообщение" required></textarea>
     </div>
 
     <div class="feedback__buttons">
@@ -24,25 +24,13 @@
   </form>
 
   <?php
-  $fb_name = $_POST['feedback_name'] ?? '';
-  $fb_email = $_POST['feedback_email'] ?? '';
-  $fb_text = $_POST['feedback_text'] ?? '';
-  $fb_date = date('Y'.'-'.'m'.'-'.'d');
-  $fb_time = date('H'.':'.'i');
-
-  //when required fields are not null
-  if ($fb_name && $fb_email && $fb_text) {
-    createFeedBack($mysqli,
-    "INSERT INTO feedbacks (name, email, comment, date, time)
-    VALUES
-    ('$fb_name', '$fb_email', '$fb_text', '$fb_date', '$fb_time');");
-  }
-
+  //get last six feedbacks from database
   $lastSix_fb = getArrayFromMySQL($mysqli, 'SELECT * FROM `feedbacks` ORDER BY `id` DESC LIMIT 0, 6');
   ?>
 
   <div class="taskCard__feedback">
     <?php
+    //post last six feedbacks from database
     showLastFeedbacks($lastSix_fb);
     ?>
   </div>
