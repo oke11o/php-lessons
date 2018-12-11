5f65aff80f1c4609405036af3d2893564a288db9
DirtyVbif <funkybboy@mail.ru>
2018-12-11 12:18:15 +0300

    modified SQL-phrase for rendering gallery

--- public/content/gallery-house1.php
+++ public/content/gallery-house1.php
@@ -9,17 +9,10 @@ include '../modules/gallery-nav.php';
   <div class="taskCard__gallery">
     <?php
     //'names' => ['name 1', ... 'name n'], 'src' => ['path 1', ... 'path n']
-    $images_house1_array = getArrayFromMySQL($mysqli, '`gallery`', '`type`', '"house1"');
+    $images_house1_array = getArrayFromMySQL($mysqli, "SELECT * FROM `gallery` WHERE `type` LIKE 'house1%' ORDER BY `views` DESC");
 
     //call rendering function of images on this page
     renderImagesFromMySQL($images_house1_array);
-
-    //call function to count number of views for this gallery
-    // $house1_array_length = count($images_house1_array['id']);
-
-    // for ($house1_i = (int)$images_house1_array['id'][0]; $house1_i <= $house1_array_length; $house1_i++) {
-    //   clickCounter($mysqli, $house1_i);
-    // }
     ?>
   </div>  
 </section>
--- public/content/gallery-house2.php
+++ public/content/gallery-house2.php
@@ -9,17 +9,10 @@ include '../modules/gallery-nav.php';
   <div class="taskCard__gallery">
     <?php
     //'names' => ['name 1', ... 'name n'], 'src' => ['path 1', ... 'path n']
-    $images_house2_array = getArrayFromMySQL($mysqli, '`gallery`', '`type`', '"house2"');
+    $images_house2_array = getArrayFromMySQL($mysqli, "SELECT * FROM `gallery` WHERE `type` LIKE 'house2%' ORDER BY `views` DESC");
 
     //call rendering function of images on this page
-    renderImagesFromMySQL($images_house2_array); 
-
-    //call function to count number of views for this gallery
-    // $house2_array_length = count($images_house2_array['id']);
-
-    // for ($house2_i = (int)$images_house2_array['id'][0]; $house2_i <= ($house2_array_length + $images_house2_array['id'][0]); $house2_i++) {
-    //   clickCounter($mysqli, $house2_i);
-    // }
+    renderImagesFromMySQL($images_house2_array);
     ?>
   </div>
 </section>
