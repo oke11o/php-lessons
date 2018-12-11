894fc7e78064d54f546b99fca45607017aa2bf88
DirtyVbif <funkybboy@mail.ru>
2018-12-11 16:27:50 +0300

    template to post feedbacks

---
+++ modules/show-feedbacks.php
@@ -0,0 +1,13 @@
+<?php
+function showLastFeedbacks($array)
+{
+  $arrayLength = count($array['id']);
+
+  for ($i = 0; $i < $arrayLength; $i++) {
+    echo('<blockquote cite="'.$array['names'][$i].'" class="taskCard__feedbackPost">');
+    echo('<p>'.$array['comment'][$i].'</p>');
+    echo('<span><cite>'.$array['names'][$i].'</cite>');
+    echo('<sub>'.$array['date'][$i].' '.$array['time'][$i].'<sub></span>');
+    echo('</blockquote>');
+  }
+}
