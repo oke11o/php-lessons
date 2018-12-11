7706bb5a1945cf8befc64b2e9f9399005b058323
DirtyVbif <funkybboy@mail.ru>
2018-12-11 22:19:58 +0300

    renamed column for message as 'description'

--- core/feedback-create.php
+++ core/feedback-create.php
@@ -10,7 +10,7 @@ if ($fb_name && $fb_email && $fb_text) {
   header('Location: ?page=feedback');
   
   mysqli_query($mysqli,
-  "INSERT INTO feedbacks (name, email, comment, date, time)
+  "INSERT INTO feedbacks (name, email, description, date, time)
   VALUES
   ('$fb_name', '$fb_email', '$fb_text', '$fb_date', '$fb_time');");
 }
