ad3f440c5ac34605c17ea70a9876414f489b172c
DirtyVbif <funkybboy@mail.ru>
2018-12-11 16:47:50 +0300

    function to create post in database

---
+++ core/feedback-create.php
@@ -0,0 +1,16 @@
+<?php
+$fb_name = $_POST['feedback_name'] ?? '';
+$fb_email = $_POST['feedback_email'] ?? '';
+$fb_text = $_POST['feedback_text'] ?? '';
+$fb_date = date('Y'.'-'.'m'.'-'.'d');
+$fb_time = date('H'.':'.'i');
+
+//when required fields are not null
+if ($fb_name && $fb_email && $fb_text) {
+  header('Location: ?page=feedback');
+  
+  mysqli_query($mysqli,
+  "INSERT INTO feedbacks (name, email, comment, date, time)
+  VALUES
+  ('$fb_name', '$fb_email', '$fb_text', '$fb_date', '$fb_time');");
+}
