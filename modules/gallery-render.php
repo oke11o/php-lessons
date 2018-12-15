3adef328c2506a114fc6e3f07c58f7da261a15c1
DirtyVbif <funkybboy@mail.ru>
2018-12-15 15:05:24 +0300

    modified gallery render

--- modules/gallery-render.php
+++ modules/gallery-render.php
@@ -3,14 +3,15 @@ function renderProducts($connection)
 {
   $products = getProducts($connection);
 
-  foreach ($products as $product) {
-    $product_DOMdiv = '<a href="?page=product&product_id='.$product['productID'].'" class="product">';
-    $product_DOMimg = '<img src="'.$product['src'].'" class="product__img">';
-    $product_DOMname = '<h3 class="product__name">'.$product['name'].'</h3>';
-    $product_DOMdescription = '<p class="product__info">'.$product['description'].'</p>';
-    $product_DOMprice = '<span class="product__price">$ '.$product['price'].'</span>';
-    $product_DOMdivEnd = '</a>';
+  foreach ($products as $product):
+  ?>
 
-    echo($product_DOMdiv.$product_DOMimg.$product_DOMname.$product_DOMdescription.$product_DOMprice.$product_DOMdivEnd);
-  }
-}
+    <a href="?page=product&product_id=<?=$product['productID']?>" class="product">
+      <img src="<?=$product['src']?>" class="product__img">
+      <h3 class="product__name"><?=$product['name']?></h3>
+      <p class="product__info"><?=$product['description']?></p>
+      <span class="product__price">$ <?=$product['price']?></span>
+    </a>
+
+  <?php endforeach;
+} 
