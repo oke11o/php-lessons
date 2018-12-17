3647fc66274448bc16cb4c50275fb975ebf0026a
DirtyVbif <funkybboy@mail.ru>
2018-12-17 19:59:52 +0300

    renamed key

--- config/init.config.php
+++ config/init.config.php
@@ -2,7 +2,7 @@
 //connection to database
 $mysql_config = include_once '../config/config.mysql.php';
 
-$mysqli = mysqli_connect($mysql_config['db-localhost'],
+$mysqli = mysqli_connect($mysql_config['db-host'],
   $mysql_config['db-user'],
   $mysql_config['db-password'],
   $mysql_config['db-database']
