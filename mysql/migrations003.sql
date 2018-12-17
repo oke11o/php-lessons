f96435845cd95e92ff940081fb60cd3009462256
DirtyVbif <funkybboy@mail.ru>
2018-12-17 20:03:16 +0300

    fixed migrations

--- mysql/migrations003.sql
+++ mysql/migrations003.sql
@@ -2,7 +2,7 @@ CREATE TABLE `feedbacks`
 (
 	`id` INT(11) NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(256) NOT NULL DEFAULT '' COMMENT 'author name',
-  `email` VARCHAR(256) NOT NULL DEFAULT '' COMMENT 'author email adress';
+  `email` VARCHAR(256) NOT NULL DEFAULT '' COMMENT 'author email adress',
   `description` TEXT NOT NULL COMMENT '',
   `date` VARCHAR(256) NOT NULL DEFAULT '' COMMENT 'date of post',
   `time` VARCHAR(256) NOT NULL DEFAULT '' COMMENT 'time of post',
